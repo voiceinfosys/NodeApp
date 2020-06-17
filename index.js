ec27243f22f2ba2dbc9327369f73727875b5edd3
voiceinfosys <61819370+voiceinfosys@users.noreply.github.com>
2020-06-17 14:58:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-//importing node framework
-var express = require(‘express’);
- 
-var app = express();
-//Respond with "hello world" for requests that hit our root "/"
-app.get(‘/hello’, function (req, res) {
- res.send(‘Hello NodeApp’);
-});
-//listen to port 3000 by default
-app.listen(process.env.PORT || 3000);
- 
-module.exports = app;
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(5000);
