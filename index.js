47bead1e7fca38486f28e79b00e9ea715527f007
voiceinfosys <61819370+voiceinfosys@users.noreply.github.com>
2020-06-17 15:04:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(5000);
+console.log('This example is different!');
